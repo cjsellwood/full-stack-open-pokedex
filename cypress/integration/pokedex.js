e22d28e388bbdb6aa3e7dc7ef7cc33955d006eb3
cjsellwood <c.sellwood22@gmail.com>
2021-08-11 15:37:02 +1000

    11.9 complete

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,4 +6,10 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+  it("allows clicking on a single pokemon", () => {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur").click();
+    cy.wait(1000);
+    cy.contains("Chlorophyll", { matchCase: false });
+  });
 });
